98a0d491a8ca250d0c221db23a9c05e078f01a89
kooi nam ng <kooinamng@koois-MacBook-Pro.local>
2020-03-03 16:25:29 +0800

    .

--- lib/stringex/acts_as_url/adapter/active_record.rb
+++ lib/stringex/acts_as_url/adapter/active_record.rb
@@ -1,23 +1,23 @@
-module Stringex
-  module ActsAsUrl
-    module Adapter
-      class ActiveRecord < Base
-        def self.load
-          ensure_loadable
-          orm_class.send :include, ActsAsUrlInstanceMethods
-          orm_class.send :extend, ActsAsUrlClassMethods
-        end
+# module Stringex
+#   module ActsAsUrl
+#     module Adapter
+#       class ActiveRecord < Base
+#         def self.load
+#           ensure_loadable
+#           orm_class.send :include, ActsAsUrlInstanceMethods
+#           orm_class.send :extend, ActsAsUrlClassMethods
+#         end
 
-      private
+#       private
 
-        def klass_previous_instances(&block)
-          klass.where(settings.url_attribute => [nil, '']).find_each(&block)
-        end
+#         def klass_previous_instances(&block)
+#           klass.where(settings.url_attribute => [nil, '']).find_each(&block)
+#         end
 
-        def self.orm_class
-          ::ActiveRecord::Base
-        end
-      end
-    end
-  end
-end
+#         def self.orm_class
+#           ::ActiveRecord::Base
+#         end
+#       end
+#     end
+#   end
+# end
